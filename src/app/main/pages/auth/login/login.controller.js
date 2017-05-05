722285492cb63e66f08c4ea1ac448435e62fa557
paharo <potopelao2@gmail.com>
2017-05-05 16:04:48 -0400

    listo el login con almacenado de token

--- src/app/main/pages/auth/login/login.controller.js
+++ src/app/main/pages/auth/login/login.controller.js
@@ -4,18 +4,46 @@
   angular.module('app.pages.auth.login').controller('LoginController', LoginController);
 
   /** @ngInject */
-  function LoginController($state, $timeout) {
+  function LoginController($state, $timeout, $http) {
     var vm = this;
 
     vm.processing = false;
     vm.login = login;
 
     function login() {
-      vm.processing = true;
-      $timeout(function() {
+      let data = {
+        email: this.form.email,
+        password: this.form.password
+      };
+      let req = {
+            method: 'POST',
+            url: 'http://digitalcook.info:8000/api/login',
+            headers: {
+              'Access-Control-Allow-Origin': '*'
+            },
+            data: data
+          }
+
+      $http(req).then(function successCallback(response){
+        localStorage.setItem("token", response.data.token);
+        console.log('request successfull & token stored');
+
+        vm.processing = true;
         this.go('app.autos');
         vm.processing = false;
-      }.bind($state), 1000);
+
+      }, function errorCallback(response){
+        console.log(response);
+      });
+
     }
   }
 })();
+
+
+/*
+vm.processing = true;
+$timeout(function() {
+        this.go('app.autos');
+        vm.processing = false;
+      }.bind($state), 1000);*/
