2d49a22171cd0c274d3d0b169d2ddeb1fd39999c
Oswald <oswald.capriles46@gmail.com>
2017-06-06 02:58:27 -0400

    Agregado el tema del zooooom para las paradas

--- src/app/main/autos/marcas/dialogs/create-form/create-form.controller.js
+++ src/app/main/autos/marcas/dialogs/create-form/create-form.controller.js
@@ -11,6 +11,11 @@
 
     vm.close = close;
     vm.save = save;
+
+		// El tema del zoooooom
+		vm.updateSubZone = updateSubZone;
+		vm.polygon = null;
+
     vm.creating = false;
 
     vm.subzonas = subzonas;
@@ -34,6 +39,59 @@
     function close() {
       $mdDialog.cancel('closed-manually');
     }
+
+		function updateSubZone(sZone) {
+			console.log(sZone);
+			let map = uniqueStopMap;
+			let fixedPoints = getBounds(sZone);
+			let p = calcCenter(sZone);
+			let center = new google.maps.LatLng(p); 
+		
+			if (vm.polygon) vm.polygon.setMap(null);
+			vm.polygon = new google.maps.Polygon({
+				paths: fixedPoints,
+				strokeColor: '#0000FF',
+				strokeOpacity: 0.8,
+				strokeWeight: 2,
+				fillColor: '#0000FF',
+				fillOpacity: 0.35
+			});
+			map.setCenter(center);
+			map.setZoom(7);
+			vm.polygon.setMap(map);
+		}
+
+		function getBounds(pol) {
+			let points = pol.poligono.linestrings[0].points;
+			let fixedPoints = [];
+			for (let i=0; i < points.length; i++) {
+				fixedPoints.push({
+					lat: points[i].lat,
+					lng: points[i].lon
+				});
+			}
+
+			return fixedPoints;
+		}
+
+		function calcCenter(pol) {
+			let points = pol.poligono.linestrings[0].points;
+			let coord = { lat: 0, lng: 0 };
+
+			coord = points.reduce(function(acc, val) {
+				acc.lat += val.lat;
+				acc.lng += val.lon; // No debe ser lon!!!
+				return acc;
+			}, {
+				lat: 0,
+				lng: 0
+			});
+
+			if (coord.lat != 0) coord.lat /= points.length;
+			if (coord.lng != 0) coord.lng /= points.length;
+
+			return coord;
+		}
   }
 
 
